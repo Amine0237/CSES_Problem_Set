05b186cf1a43c765b083deddbe1da7e99f7ab415
Amine0237 <chadli.medamin@gmail.com>
2023-07-12 13:54:11 +0100

    first commit

---
+++ test.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef double d;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+#define FOR(i,a,b) for(int i=a; i<b; i++)
+#define p(a) cout << a
+
+
+int main(){
+
+	/*
+	getline(cin, s); //including spaces
+
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	
+	printf("%.9f\n",x); // prints the value of x with 9 decimal places
+
+	if(abs(a-b)<1e-9) //a and b are equal
+	
+	vector<int> v = {4,2,5,3,5,8,3};
+	sort(v.rbegin(), v.rend());
+
+	for(auto i = v.begin(); i!=v.end(); i++){
+		cout << *i;
+	}
+	
+	vector<pair<int,int>> v;
+	v.push_back({1,7});
+	v.push_back({1,9});
+	v.push_back({0,5});
+	v.push_back({2,10});
+
+	for(auto i=v.begin(); i!=v.end(); i++){
+		cout << (*i).first << " " << (*i).second << "\n";
+	}
+	
+	struct P {
+		int x, y;
+		bool operator<(const P &p) {
+			if(x!=p.x) return x<p.x;
+			else return y<p.y;
+		}
+	};
+
+	P p1, p2;
+	p1.x = 5; p1.y = 6;
+	p2.x = 4; p2.y = 5;
+	
+	if(p1<p2) cout << "yes";
+	else cout << "no";
+
+	bool comp(string a, string b) {...}
+	sort(v.begin(), v.end(), comp);
+
+	int array[5] = {3,7,12,16,23};
+	int x = 12;
+
+	// binary search in a fancy cpp way
+	auto k = lower_bound(array, array+5, x)-array; // there is also upper_bound, equal_range
+	if(k<5 && array[k] == x){
+		cout << "x found at index " << k;
+	}	
+
+	// counting the number of elements whose value is x
+	int array[7] = {3,7,12,12,12,16,23};
+	int x = 12;
+	auto a = lower_bound(array, array+7, x); 
+	auto b = upper_bound(array, array+7, x);
+	cout << b-a;
+	// or just do :
+	auto r = equal_range(array, array+7, x);
+	cout << r.second - r.first;
+	
+
+*/
+
+	
+
+
+
+}
